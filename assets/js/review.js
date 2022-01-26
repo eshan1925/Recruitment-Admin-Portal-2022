40f36449078fac9674870c9319807df5885b8d62
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-01-26 22:45:16 +0530

    reviewEdited

---
+++ assets/js/review.js
@@ -0,0 +1,29 @@
+function selectCategory() {
+    var category = document.getElementById("category");
+    // console.log(category);
+
+    var selectedCategory = category.options[category.selectedIndex].text;
+    let element1 = document.getElementById("user_section");
+
+    let element2 = document.getElementById("question_section");
+
+
+
+    let element3 = document.getElementById("rightQuestionFirst");
+    let element4 = document.getElementById("rightQuestionSecond");
+
+    switch (selectedCategory) {
+        case "User":
+            element1.classList.remove("hidden");
+            element3.classList.remove("hidden");
+            element2.classList.add("hidden");
+            element4.classList.add("hidden");
+            break;
+        case "Questions":
+            element1.classList.add("hidden");
+            element3.classList.add("hidden");
+            element2.classList.remove("hidden");
+            element4.classList.remove("hidden");
+            break;
+    }
+}
