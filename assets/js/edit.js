24e53a2d0498c55966e9f5c1619c5479375dbb7b
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-01-27 11:33:30 +0530

    DropDownEditPage

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -7,3 +7,43 @@ function btnSave() {
         document.getElementById("change").innerHTML = "Save Changes";
     }
 }
+
+
+
+function selectCategory() {
+    var category = document.getElementById("category");
+    var selectedCategory = category.options[category.selectedIndex].text;
+    console.log(selectedCategory);
+    let e1 = document.getElementById("all");
+    let e2 = document.getElementById("easy");
+    let e3 = document.getElementById("medium");
+    let e4 = document.getElementById("hard");
+
+    switch (selectedCategory) {
+        case "All":
+            e1.classList.remove("hidden");
+            e2.classList.add("hidden");
+            e3.classList.add("hidden");
+            e4.classList.add("hidden");
+            break;
+        case "Easy":
+            e1.classList.add("hidden");
+            e2.classList.remove("hidden");
+            e3.classList.add("hidden");
+            e4.classList.add("hidden");
+            break;
+        case "Medium":
+            e1.classList.add("hidden");
+            e2.classList.add("hidden");
+            e3.classList.remove("hidden");
+            e4.classList.add("hidden");
+            break;
+        case "Hard":
+            e1.classList.add("hidden");
+            e3.classList.add("hidden");
+            e2.classList.add("hidden");
+            e4.classList.remove("hidden");
+            break;
+
+    }
+}
