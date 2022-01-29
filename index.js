6433af7e8df6f12ea8b3efb107f28b54eff00259
eshan1925 <eshan1925@gmail.com>
2022-01-30 00:50:16 +0530

    routing fixes

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.set('view engine','ejs');
 
 app.use(express.static(__dirname + "/public"));
 
+//localhost:3000  www.xyz.com/domain
 app.get("/",function(req,res){
-    res.render("edit.ejs");
+    res.render("login.ejs");
 });
 
 app.get("/domain",function(req,res){
@@ -28,6 +29,14 @@ app.get("/upload",function(req,res){
     res.render("upload.ejs");
 });
 
+app.get("/edit",function(req,res){
+    res.render("edit.ejs");
+});
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
+});
+
+app.post("/",function (req,res) {
+    
 });
