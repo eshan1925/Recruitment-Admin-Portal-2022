275727a384c0d3a02a6c92c4a940ff3139b2df7d
eshan1925 <eshan1925@gmail.com>
2022-01-30 03:36:06 +0530

    Login Functionality

--- index.js
+++ index.js
@@ -1,42 +1,75 @@
+
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
+var bodyParser = require('body-parser');
+var axios = require('axios');
 
 const app = express();
 
-app.set('view engine','ejs');
-
-
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 //localhost:3000  www.xyz.com/domain
-app.get("/",function(req,res){
+
+app.get("/", function (req, res) {
     res.render("login.ejs");
 });
 
-app.get("/domain",function(req,res){
+app.get("/domain", function (req, res) {
     res.render("domain.ejs");
 });
 
-app.get("/result",function(req,res){
+app.get("/result", function (req, res) {
     res.render("result.ejs");
 });
 
-app.get("/review",function(req,res){
+app.get("/review", function (req, res) {
     res.render("review.ejs");
 });
 
-app.get("/upload",function(req,res){
+app.get("/upload", function (req, res) {
     res.render("upload.ejs");
 });
 
-app.get("/edit",function(req,res){
+app.get("/edit", function (req, res) {
     res.render("edit.ejs");
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
 
-app.post("/",function (req,res) {
-    
+app.post("/", function (req, res) {
+    console.log(req.body);
+    if (req.body.email === process.env.ADMIN_EMAIL && req.body.password === process.env.ADMIN_PASSWORD) {
+        var credentials = JSON.stringify({
+            "email": process.env.ADMIN_EMAIL,
+            "password": process.env.ADMIN_PASSWORD
+        });
+        var config = {
+            method: 'post',
+            url: 'https://damp-river-26250.herokuapp.com/admin/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: credentials
+        };
+
+        axios(config)
+            .then(function (response) {
+                var activeCookie = response.data.Token;
+                console.log(activeCookie);
+                res.redirect("/domain");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });        
+    } else {
+        console.log("Unauthorised User!!!");
+        res.redirect("/");
+    }
 });
+
+
